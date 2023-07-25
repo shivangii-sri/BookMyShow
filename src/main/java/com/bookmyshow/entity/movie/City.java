e5ae1d8b85bcce554bfd1a186638280e2da49544
shivangii-sri <shivangisrivastava762@gmail.com>
2023-07-25 13:08:55 +0530

    Created city entity

--- src/main/java/com/bookmyshow/entity/movie/City.java
+++ src/main/java/com/bookmyshow/entity/movie/City.java
@@ -1,4 +1,39 @@
 package com.bookmyshow.entity.movie;
 
-public class City {
+import jakarta.persistence.*;
+
+@Entity
+@Table(name= "city")
+public class City{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String state;
+    private String zipCode;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
 }
