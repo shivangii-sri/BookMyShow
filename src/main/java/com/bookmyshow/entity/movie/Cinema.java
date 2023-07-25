2972dda7d393cc676bd90c5fd59c8e22c01fc9db
sudhanshujaiswal <jaiswalsudhanshu20@gmail.com>
2023-07-25 12:40:23 +0530

    feat : added entities of movie package

---
+++ src/main/java/com/bookmyshow/entity/movie/Cinema.java
@@ -0,0 +1,11 @@
+package com.bookmyshow.entity.movie;
+
+import java.util.List;
+
+public class Cinema {
+
+    private String name;
+    private int totalCinemaHalls;
+
+    private List<CinemaHall> halls;
+}
