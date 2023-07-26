066378c08e68d15dbae94f09331efb5e6c628974
shivangii-sri <shivangisrivastava762@gmail.com>
2023-07-27 01:24:12 +0530

    feat : implemented Account entity with ENUM type column

--- src/main/java/com/bookmyshow/entity/users/Account.java
+++ src/main/java/com/bookmyshow/entity/users/Account.java
@@ -1,10 +1,22 @@
 package com.bookmyshow.entity.users;
 
+import com.bookmyshow.entity.common.BaseEntity;
 import com.bookmyshow.enums.AccountStatus;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
-public class Account {
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "account")
+public class Account extends BaseEntity {
 
-    private String id;
     private String password;
-    private AccountStatus status;
+
+    @Enumerated(EnumType.STRING)
+    private AccountStatus accountStatus;
 }
